8571ecd2d908d2c556c6317cf6c44de6c3ae6b9a
ishan-surana <ishan5.mitmpl2022@learner.manipal.edu>
2024-03-31 20:24:33 +0530

    Added Lab 5, 7 and 8

---
+++ Lab Codes/Sem 4/DBS/VS 2012 Working Codes (Lab 1 and 2)/Lab 7/Insurance/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Insurance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new Insurance.DataSet1();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.aCCIDENTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aCCIDENTTableAdapter = new Insurance.DataSet1TableAdapters.ACCIDENTTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aCCIDENTBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 126);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.dataSet1BindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(118, 176);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSet1BindingSource
+            // 
+            this.dataSet1BindingSource.DataSource = this.dataSet1;
+            this.dataSet1BindingSource.Position = 0;
+            // 
+            // dataSet1BindingSource1
+            // 
+            this.dataSet1BindingSource1.DataSource = this.dataSet1;
+            this.dataSet1BindingSource1.Position = 0;
+            // 
+            // aCCIDENTBindingSource
+            // 
+            this.aCCIDENTBindingSource.DataMember = "ACCIDENT";
+            this.aCCIDENTBindingSource.DataSource = this.dataSet1BindingSource1;
+            // 
+            // aCCIDENTTableAdapter
+            // 
+            this.aCCIDENTTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 417);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aCCIDENTBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private System.Windows.Forms.BindingSource aCCIDENTBindingSource;
+        private DataSet1TableAdapters.ACCIDENTTableAdapter aCCIDENTTableAdapter;
+    }
+}
+
---
+++ Lab Codes/Sem 4/DBS/VS 2012 Working Codes (Lab 1 and 2)/Lab 7/Insurance/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using System.Data.SqlClient;
+using System.Configuration;   
+
+namespace Insurance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String oradb = "DATA SOURCE=172.16.54.24:1521/ictorcl;USER ID=IT478;Password=student";
+            OracleConnection conn = new OracleConnection(oradb);
+            conn.Open();
+            OracleDataAdapter da;
+            DataSet ds;
+            DataTable dt;
+            DataRow dr;
+            int i = 0;
+            MessageBox.Show("Connected");
+
+            OracleCommand command1 = conn.CreateCommand();
+            command1.CommandText = "select name as Owner, count(report_number) as No_Of_Accidents, sum(damage_amount) as total_damage from person, participated where person.driver_id = participated.driver_id group by(person.driver_id,name)";
+            command1.CommandType = CommandType.Text;
+
+            ds = new DataSet();
+            da = new OracleDataAdapter(command1.CommandText, conn);
+            da.Fill(ds, "Accident");
+            dt = ds.Tables["Accident"];
+            int t = dt.Rows.Count;
+            MessageBox.Show(t.ToString());
+            textBox1.Text = "";
+            for (i = 0; i < t; i++)
+            {
+                dr = dt.Rows[i];
+                string owner = dr["owner"].ToString();
+                string noa = dr["no_of_accidents"].ToString();
+                string td = dr["total_damage"].ToString();
+                string line = owner + ", " + noa + ", " + td + "\r\n";
+                textBox1.AppendText(line);
+            }
+
+            OracleDataReader reader = command1.ExecuteReader();
+            dataGridView1.DataSource = reader;
+            dataGridView1.DataBind();
+            command1.Dispose();
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.ACCIDENT' table. You can move, or remove it, as needed.
+            this.aCCIDENTTableAdapter.Fill(this.dataSet1.ACCIDENT);
+
+        }
+    }
+}
